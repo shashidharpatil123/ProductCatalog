b5685bdcdbc7df7fe97d4a7f452921fe5f6d0b35
Sandeep Soni <sandeepsoni@deccansoft.com>
2021-08-14 11:21:39 +0000

    Updated Program.cs

--- ProductCatalog.Web/Program.cs
+++ ProductCatalog.Web/Program.cs
@@ -20,7 +20,7 @@ namespace ProductCatalog.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
